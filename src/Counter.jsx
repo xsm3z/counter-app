55d62704b0f9597178406cebcb19cb65ca1a2756
xsm3z <xsm.zhang@gmail.com>
2024-09-03 19:42:45 -0400

    Added a clear and dynamic color for count

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,40 +3,50 @@ import Button from "./Button";
 
 function Counter() {
   const [value, setValue] = useState(0);
+  const [inputValue, setInputValue] = useState(0);
 
-  const handleIncrement = (incrementValue) => {
-    const newValue = value + parseInt(incrementValue, 10);
-    setValue(newValue);
+  const handleIncrement = () => {
+    setValue(value + parseInt(inputValue, 10));
   };
 
-  const handleDecrement = (decrementValue) => {
-    const newValue = value - parseInt(decrementValue, 10);
-    setValue(newValue);
+  const handleDecrement = () => {
+    setValue(value - parseInt(inputValue, 10));
+  };
+
+  const handleClear = () => {
+    setValue(0);
+    setInputValue(0);
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const getColor = () => {
+    if (value > 0) return "green";
+    if (value < 0) return "red";
+    return "black";
   };
 
   return (
     <div>
       <h1>Please Add or Subtract</h1>
       <div className="board">
-        <div id="value" style={{ color: value < 0 ? "red" : "black" }}>
+        <div id="value" style={{ color: getColor() }}>
           {value}
         </div>
         <div className="container">
-          <Button
-            action={() =>
-              handleIncrement(document.getElementById("number").value)
-            }
-            symbol="+"
-          />
           <div className="value-form">
-            <input type="number" id="number" defaultValue="0" />
+            <input
+              type="number"
+              value={inputValue}
+              onChange={handleInputChange}
+              style={{ padding: "10px", fontSize: "16px" }}
+            />
+            <Button action={handleClear} symbol="Clear" />
           </div>
-          <Button
-            action={() =>
-              handleDecrement(document.getElementById("number").value)
-            }
-            symbol="-"
-          />
+          <Button action={handleDecrement} symbol="-" />
+          <Button action={handleIncrement} symbol="+" />
         </div>
       </div>
     </div>
